c3bf2388c42372ab6f5c197e3df42407aedbac7c
srinidhichinthakindi-svg <srinidhichinthakindi@gmail.com>
2026-03-31 12:17:51 +0530

    need to add some  code
    i need to add remaning code

---
+++ app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import joblib
+import pandas as pd
+import numpy as np
+model=joblib.load("C:\\Users\\SRINIDHI CH\\OneDrive\\Desktop\\vscode\\employee_attrition_model.pkl")
+label_encoder=joblib.load("C:\\Users\\SRINIDHI CH\\OneDrive\\Desktop\\vscode\\label_encoder.pkl")
+feature_columns=joblib.load("C:\\Users\\SRINIDHI CH\\OneDrive\\Desktop\\vscode\\feature_columns.pkl")
+st.title("Employee Atrition Prediction")
+st.markdown("Enter the employee details to predit whether they are likely to leave the company.")
+st.sidebar.header("Employee Details")
+def user_input_features():
+    inputs={}
+    inputs['Age']=st.sidebar.number_input("Age", min_value=18, max_value=65, value=30)
+    inputs['MonthlyIncome']=st.sidebar.number_input("Monthly Income", min_value=1000,
+                         max_value=20000, value=5000)
+    inputs['JobSatisfaction']=st.sidebar.selectbox("Job Satisfaction",
+                                                    [1, 2, 3, 4])
+    inputs['OverTime']=st.sidebar.selectbox("Over Time",
+                                            ['Yes', 'No'])
+    inputs['DistanceFromHome']=st.sidebar.number_input("Distance From Home", min_value=0, 
+                                                       max_value=50, value=10)
+    
+    data={}
+    for feat in feature_columns:
+        if feat in inputs:
+            data[feat]=inputs[feat]
+        else:
+            data[feat]=0
+    return pd.DataFrame(data, index=[0])
+input_df=user_input_features()
+input_df['OverTime']=label_encoder.transform(input_df['OverTime'])
+if st.button("Predict Attrition"):
+    prediction=model.predict(input_df)
+    if prediction[0]==1:
+        st.error("The employee is likely to leave the company.")
+    else:
+        st.success("The employee is likely to stay with the company.")
+if st.button("Predict Probability"):
+    proba=model.predict_proba(input_df)
+    prediction_parabola=model.predict(input_df)
+    st.subheader("Prediction Probability")
+    if prediction==1:
+        st.error("The employee is likely to leave the company.")
+    else:
+        st.success("The employee is likely to stay with the company.")
+    st.subheader("Prediction Probability")
+    st.write(f"Probability of leaving: {proba[0][1]:.2f}")
+    st.write(f"Probability of staying: {proba[0][0]:.2f}")
